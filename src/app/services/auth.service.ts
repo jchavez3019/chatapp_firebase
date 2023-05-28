5c9085686fd9ee0da21d4bb2d5bccc2b48398dab
jorgejc2 <jorgejc2@illinois.edu>
2023-05-28 15:45:36 -0500

    closer to getting old firebase api to work

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,13 +22,52 @@ export class AuthService {
 
   /* sign up */
   signUp(usercreds: any) {
+
+    /* DEBUG */
+    console.log("Logging in with the user: " + usercreds);
+    console.log("email: " + usercreds.email + " pwd: " + usercreds.password);
+
+    /* from the Firebase documentation */
+    // return this.afauth.createUserWithEmailAndPassword(usercreds.email, usercreds.password)
+    // .then((userCredential) => {
+
+    //   /* at this point, the user is signed in and firebase has returned the user's new credentials */
+    //   const user = userCredential.user;
+
+    //   /* rest of user signed in code */
+    //   // ...
+    // })
+    // .catch((error) => {
+    //   /* basic error checking */
+    //   const errorCode = error.code;
+    //   const errorMessage = error.message;
+
+    //   /* rest of error code */
+    //   // ...
+    // })
+
+    /* from the tutorial */
     return this.afauth.createUserWithEmailAndPassword(usercreds.email, usercreds.password)
-    .then((user) => {
-      this.authState = user;
+    .then((userCredential) => {
+      /* at this point the user is signed in and the new user is returned as userCredential */
+      console.log(userCredential);
+      this.authState = userCredential;
+
+      /* udpating with tutorial */
       this.updateProfile(usercreds.displayName);
+
+      /* updating using firebase documentation */
+      // this.afauth.updateCurrentUser
       
     })
+    .catch((error) => {
+      /* one error to check is the case where the user already exists */
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("error code: " + errorCode + " with msg: " + errorMessage);
+    })
     .then(() => {
+      /* now update this user locally */
       this.setUserData(usercreds.email, usercreds.displayName, usercreds.photoURL);
     });
   }
@@ -43,6 +82,10 @@ export class AuthService {
 
   /* set user data to a local users collections used to reference throughout the application */
   setUserData(email: string, displayName: string, photoURL: string) {
+    /* DEBUG */
+    console.log("Getting user from db");
+
+    /* this returns information about the user from firestore */
     const path = `users/${this.currentUserId}`;
     const statuspath = `status/${this.currentUserId}`;
     const userdoc: any = this.afs.doc(path);
