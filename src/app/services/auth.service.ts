ad3c0309ac225158187393a4253bae239e491ca2
jorgejc2 <jorgejc2@illinois.edu>
2023-08-28 23:17:56 -0500

    changes show real time status updates in firebase, next is to reflect this in the UI as well

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, OnDestroy, Query, inject } from '@angular/core';
 import { Auth, authState, User, user, createUserWithEmailAndPassword, UserCredential, updateProfile, AuthSettings, signInWithEmailAndPassword, signOut, UserProfile, onAuthStateChanged, Unsubscribe } from '@angular/fire/auth';
 import { Firestore, setDoc, doc, query, collection, where, getDocs, QuerySnapshot, updateDoc, DocumentReference, FirestoreError, addDoc } from '@angular/fire/firestore';
-import { Database, OnDisconnect, onDisconnect, ref } from '@angular/fire/database';
+import { DataSnapshot, Database, DatabaseReference, OnDisconnect, onDisconnect, onValue, ref, set } from '@angular/fire/database';
 import { Router } from '@angular/router';
 
 /* firebase data types */
 import { UserData, UserStatus, userDataConverter, userStatusConverter } from '../firestore.datatypes';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { emit } from 'process';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,8 @@ export class AuthService implements OnDestroy {
 
   private auth: Auth = inject(Auth);
   private database: Database = inject(Database);
+  private onDisconnectObj: OnDisconnect | null = null;
+  private onRealtimeStatusUnsubscribe: Unsubscribe | null = null;
 
   /*
     A quick note on this practice, subjects in services should typically be private. This is because
@@ -44,6 +47,7 @@ export class AuthService implements OnDestroy {
       if (credential) {
         this.authUserSubject.next(credential);
         this.hasAuthSignedInSubject.next(true);
+        this.connectionSnapshot();
         console.log(`User has logged in with displayName ${credential.displayName} and email ${credential.email}`);
       }
       else {
@@ -61,14 +65,18 @@ export class AuthService implements OnDestroy {
       this.unsubscribeOnAuthStateChanged = null;
     }
 
+    if (this.onRealtimeStatusUnsubscribe != null) {
+      this.onRealtimeStatusUnsubscribe();
+      this.onRealtimeStatusUnsubscribe = null;
+    }
+      
+
     /* sign out the user if they logged in */
     this.logout();
   }
 
   authUserLoggedIn() : Boolean {
-    const authUserSubjectLast: User | null | undefined = this.authUserSubject.getValue();
-
-    if (typeof(authUserSubjectLast) == null || typeof(authUserSubjectLast) == undefined)
+    if (typeof(this.auth.currentUser) == null)
       return false;
     else
       return true;
@@ -77,7 +85,13 @@ export class AuthService implements OnDestroy {
   /* sign up */
   signUp(usercreds: any) : Promise<void> {
 
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
+
+        /* if a user is already logged in, log them out before creating a new user */
+        if (this.authUserLoggedIn()) {
+          await this.logout()
+          .catch((error) => reject(error));
+        }
 
         createUserWithEmailAndPassword(this.auth, usercreds.email, usercreds.password)
         .then((userCredential) => {
@@ -99,16 +113,8 @@ export class AuthService implements OnDestroy {
           /* now update the user's status */
           const prom4 = this.setStatus("online");
 
-
           Promise.all([prom1, prom2, prom3, prom4])
-          .then((allPromiseRet: [void, void, void, string]) => {
-            const statusPath = allPromiseRet[3];
-            const dbRef = ref(this.database, statusPath)
-            const disconectObj = onDisconnect(dbRef);
-            disconectObj.set({
-              "online": false
-            });
-
+          .then(() => {
             resolve();
           })
           .catch((error) => {
@@ -128,7 +134,13 @@ export class AuthService implements OnDestroy {
   /* login */
   login(usercreds: any): Promise<void> {
 
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
+
+      /* if a user is already logged in, log them out before creating a new user */
+      if (this.authUserLoggedIn()) {
+        await this.logout()
+        .catch((error) => reject(error));
+      }
 
       signInWithEmailAndPassword(this.auth, usercreds.email, usercreds.password)
       .then((userCredential) => {
@@ -136,19 +148,15 @@ export class AuthService implements OnDestroy {
         /* update the current user */
         this.auth.updateCurrentUser(user)
         .then(() => {
-          /* update the user's status */
+          /* update the user's status and realtime status */
+
           this.setStatus("online")
-          .then((statusPath: string) => {
-            console.log("Status path is: " + statusPath);
-            const dbRef = ref(this.database, statusPath);
-            
-            const disconectObj = onDisconnect(dbRef);
-            disconectObj.set({
-              "online": false
-            });
+          .then(() => {
+            // this.connectionSnapshot();
             resolve();
           })
-          .catch((error) => reject("Error setting status with message: \n" + error.message));
+          .catch((error) => reject(error));
+
         })
         .catch((error) => reject("error updating current auth user with message: \n" + error.message));
   
@@ -163,21 +171,37 @@ export class AuthService implements OnDestroy {
   }
 
   /* logout */
-  async logout(): Promise<void> {
+  logout(): Promise<void> {
 
-    return new Promise<void>(async (resolve, reject) => {
+    return new Promise<void>((resolve, reject) => {
 
-      if (this.auth.currentUser != null) {
-        /* set the status of the user to offline */
-        await this.setStatus("offline")
+      this.setStatus("offline")
+      .then(() => {
+
+        this.setRealtimeStatus("offline")
+        .then(() => {
+
+          this.cancelOnDisconnect()
+          .then(() => {
+
+            if (this.onRealtimeStatusUnsubscribe == null) {
+              reject("onRealtimeStatus snapshot was never created");
+            }
+            else {
+              this.onRealtimeStatusUnsubscribe();
+              this.onRealtimeStatusUnsubscribe = null;
+              signOut(this.auth);
+              resolve();
+            }
+            
+          })
+          .catch((error) => reject(error));
+
+        })
         .catch((error) => reject(error));
-      }
 
-      /* logout the user */
-      await signOut(this.auth)
-      .catch((error) => reject(error));
-
-      resolve();
+        
+      })
 
     });
 
@@ -211,6 +235,32 @@ export class AuthService implements OnDestroy {
       
     });
 
+  }
+
+  setRealtimeStatus(status: string) : Promise<string> {
+    return new Promise<string> ((resolve, reject) => {
+      let isOnline: Boolean = false;
+
+      if (status === "online")
+        isOnline = true;
+      else if (status === "offline")
+        isOnline = false;
+      else
+        reject("Invalid realtime status; Status should be either 'online' or 'offline'");
+
+      const newDocData = {
+        "email": this.auth.currentUser?.email,
+        "online": isOnline
+      }
+      const docPath = `status/${this.auth.currentUser?.uid}`
+      const docRef: DatabaseReference = ref(this.database, docPath);
+
+      set(docRef, newDocData).then(() => {
+        resolve(docPath);
+      })
+      .catch((error) => reject(error));
+
+    });
   }
 
   /* Updates the status of a user. If the user's status does not exist, it will be created */
@@ -265,6 +315,59 @@ export class AuthService implements OnDestroy {
 
     });
 
+  }
+
+  private connectionSnapshot() : void {
+
+    /* this function was called before, no need to recreate the realtime status snapshot */
+    if (this.onRealtimeStatusUnsubscribe != null) {
+      return;
+    }
+
+    this.onRealtimeStatusUnsubscribe = onValue(ref(this.database, '.info/connected'), (data_snapshot: DataSnapshot) => {
+      /* if the user is offline, return */
+      if (data_snapshot.val() === 'false') 
+        return;
+      
+
+      const dbRef = ref(this.database, `status/${this.auth.currentUser?.uid}`);
+      this.onDisconnectObj = onDisconnect(dbRef);
+      this.onDisconnectObj.set({
+        "email": this.auth.currentUser?.email,
+        "online": false
+      })
+      .then(() => {
+        this.setRealtimeStatus('online');
+      })
+
+    }, 
+    (error: Error) => {
+      console.error(error);
+    });
+
+    return;
+
+  }
+
+  cancelOnDisconnect() : Promise<void> {
+
+    return new Promise<void> ((resolve, reject) => {
+      if (this.onDisconnectObj != null) {
+        this.onDisconnectObj.cancel()
+        .then(() => {
+          /* after cancelling, this object to null and resolve */
+          this.onDisconnectObj = null;
+          resolve();
+        })
+        .catch((error) => reject(error));
+      }
+      else {
+        /* if object was null, onDisconnect was already cancelled so we can resolve right away */
+        resolve();
+      }
+      
+    });
+    
   }
 
 
